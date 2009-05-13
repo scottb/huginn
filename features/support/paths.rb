ee8334f1f24fa4bc6bcceb49c9dd6a80917ba7fb
Scott Brickner <scottb@brickner.net>
2009-05-13 12:35:00 -0400

    modified files

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,9 @@ module NavigationHelpers
     
     when /the homepage/
       '/'
+    when /the new occasion page/
+      new_occasion_path
+
     
     # Add more mappings here.
     # Here is a more fancy example:
